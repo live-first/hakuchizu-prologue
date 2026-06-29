defc912545f708e393ebd23614b7d8c6b32f0e89
TakedaRyota <tkdryt05@gmail.com>
2026-06-29 17:08:47 +0900

    API

--- src/api/microApi.tsx
+++ src/api/microApi.tsx
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk'
 
 export const client = createClient({
   serviceDomain: 'hcpl',
-  apiKey: 'U6fD6tXy0pNJ49lqdifnNeyLXCYXtDDLqlkK',
+  apiKey: process.env.NEXT_PUBLIC_MICROCMS_API_KEY!,
 })
